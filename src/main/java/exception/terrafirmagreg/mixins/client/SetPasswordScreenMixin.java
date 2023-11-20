bc1c13224a38311c35cc312d37ca4f66368d8219
SpeeeDCraft <52341158+SpeeeDCraft@users.noreply.github.com>
2023-11-20 22:03:51 +0700

    Revert "dead"
    This reverts commit acdb4374aed5429ce5fc0379e7daafca344f0b01.

---
+++ src/main/java/exception/terrafirmagreg/mixins/client/SetPasswordScreenMixin.java
@@ -0,0 +1,41 @@
+package exception.terrafirmagreg.mixins.client;
+
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.components.MultiLineTextWidget;
+import net.minecraft.client.gui.components.StringWidget;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import top.seraphjack.simplelogin.client.SetPasswordScreen;
+
+@SuppressWarnings("all")
+@OnlyIn(Dist.CLIENT)
+@Mixin(value = SetPasswordScreen.class, remap = false)
+public final class SetPasswordScreenMixin extends Screen {
+
+    @Unique
+    private MultiLineTextWidget terraFirmaGreg_1_20_x$textField;
+
+    private SetPasswordScreenMixin(Component pTitle) {
+        super(pTitle);
+    }
+
+    @Inject(method = "m_7856_", at = @At(value = "TAIL"), remap = false)
+    private void onInit(CallbackInfo ci) {
+        this.terraFirmaGreg_1_20_x$textField = new MultiLineTextWidget(
+                this.width / 2 - 200,
+                this.height / 3,
+                Component.translatable("tfg.simplelogin.description"), this.font).setMaxWidth(400).setCentered(true).setMaxRows(5);
+    }
+
+    @Inject(method = "m_88315_", at = @At(value = "TAIL"), remap = false)
+    private void onRender(GuiGraphics gui, int mouseX, int mouseY, float partialTicks, CallbackInfo ci) {
+        this.terraFirmaGreg_1_20_x$textField.render(gui, mouseX, mouseY, partialTicks);
+    }
+}
