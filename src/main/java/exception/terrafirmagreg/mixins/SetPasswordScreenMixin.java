cf6c1174b589a00b1f02fb156d26a9cee23ea90d
SpeeeDCraft <52341158+SpeeeDCraft@users.noreply.github.com>
2023-11-20 10:03:11 +0700

    разраб миксинов дэбил

--- src/main/java/exception/terrafirmagreg/mixins/SetPasswordScreenMixin.java
+++ src/main/java/exception/terrafirmagreg/mixins/SetPasswordScreenMixin.java
@@ -14,27 +14,28 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import top.seraphjack.simplelogin.client.SetPasswordScreen;
 
+@SuppressWarnings("all")
 @OnlyIn(Dist.CLIENT)
 @Mixin(value = SetPasswordScreen.class, remap = false)
-public abstract class SetPasswordScreenMixin extends Screen {
+public final class SetPasswordScreenMixin extends Screen {
 
     @Unique
     private MultiLineTextWidget terraFirmaGreg_1_20_x$textField;
 
-    protected SetPasswordScreenMixin(Component pTitle) {
+    private SetPasswordScreenMixin(Component pTitle) {
         super(pTitle);
     }
 
-    @Inject(method = "init", at = @At(value = "HEAD"), remap = false)
-    protected void onInit(CallbackInfo ci) {
+    @Inject(method = "m_7856_", at = @At(value = "TAIL"), remap = false)
+    private void onInit(CallbackInfo ci) {
         this.terraFirmaGreg_1_20_x$textField = new MultiLineTextWidget(
                 this.width / 2 - 200,
                 this.height / 3,
                 Component.translatable("tfg.simplelogin.description"), this.font).setMaxWidth(400).setCentered(true).setMaxRows(5);
     }
 
-    @Inject(method = "render", at = @At(value = "HEAD"), remap = false)
-    protected void onRender(GuiGraphics gui, int mouseX, int mouseY, float partialTicks, CallbackInfo ci) {
+    @Inject(method = "m_88315_", at = @At(value = "TAIL"), remap = false)
+    private void onRender(GuiGraphics gui, int mouseX, int mouseY, float partialTicks, CallbackInfo ci) {
         this.terraFirmaGreg_1_20_x$textField.render(gui, mouseX, mouseY, partialTicks);
     }
 }
