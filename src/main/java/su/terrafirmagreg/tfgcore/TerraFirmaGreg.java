7a98e116fc4e893bce8e43c2351f12b040ab3ffb
Dmitry <ondyshev.dmitry@gmail.com>
2023-12-31 19:09:18 +0700

    Добавлена поддержка Create 2

--- src/main/java/su/terrafirmagreg/tfgcore/TerraFirmaGreg.java
+++ src/main/java/su/terrafirmagreg/tfgcore/TerraFirmaGreg.java
@@ -8,6 +8,7 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
+import su.terrafirmagreg.tfgcore.compat.create.CustomArmInteractionPointTypes;
 import su.terrafirmagreg.tfgcore.compat.gtceu.TFGGTAddon;
 import su.terrafirmagreg.tfgcore.compat.tfcambiental.TFCAmbientalCompat;
 
@@ -25,6 +26,7 @@ public class TerraFirmaGreg {
 
         MinecraftForge.EVENT_BUS.register(this);
 
-        //TFCAmbientalCompat.register();
+        TFCAmbientalCompat.register();
+        CustomArmInteractionPointTypes.register();
     }
 }
---
+++ src/main/java/su/terrafirmagreg/tfgcore/mixins/common/flywheel/AnimatedBlazeBurnerMixin.java
@@ -0,0 +1,51 @@
+package su.terrafirmagreg.tfgcore.mixins.common.flywheel;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.math.Axis;
+import com.simibubi.create.compat.jei.category.animations.AnimatedBlazeBurner;
+import com.simibubi.create.compat.jei.category.animations.AnimatedKinetics;
+import com.simibubi.create.content.processing.burner.BlazeBurnerBlock;
+import net.dries007.tfc.common.blocks.TFCBlocks;
+import net.dries007.tfc.common.blocks.devices.CharcoalForgeBlock;
+import net.minecraft.client.gui.GuiGraphics;
+import org.jetbrains.annotations.NotNull;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+
+@Mixin(value = AnimatedBlazeBurner.class, remap = false)
+public abstract class AnimatedBlazeBurnerMixin extends AnimatedKinetics {
+
+    @Shadow
+    private BlazeBurnerBlock.HeatLevel heatLevel;
+
+    /**
+     * @author Exception
+     * @reason Рендерить блок угольной печи вместо блейзовой печи Create.
+     */
+    @Overwrite
+    public void draw(@NotNull GuiGraphics graphics, int xOffset, int yOffset) {
+        PoseStack matrixStack = graphics.pose();
+        matrixStack.pushPose();
+        matrixStack.translate(xOffset, yOffset, 200);
+        matrixStack.mulPose(Axis.XP.rotationDegrees(-15.5f));
+        matrixStack.mulPose(Axis.YP.rotationDegrees(22.5f));
+
+        int scale = 23;
+
+        int forgeHeatLevel = 0;
+        if (heatLevel == BlazeBurnerBlock.HeatLevel.SEETHING) {
+            forgeHeatLevel = 7;
+        } else if (heatLevel == BlazeBurnerBlock.HeatLevel.KINDLED) {
+            forgeHeatLevel = 3;
+        }
+
+        blockElement(TFCBlocks.CHARCOAL_FORGE.get().defaultBlockState().setValue(CharcoalForgeBlock.HEAT, forgeHeatLevel))
+                .atLocal(0, 1.65, 0)
+                .scale(scale)
+                .render(graphics);
+
+        matrixStack.popPose();
+    }
+
+}
