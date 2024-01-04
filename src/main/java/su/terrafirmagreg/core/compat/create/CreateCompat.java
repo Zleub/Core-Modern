c4c1a4558c9d49ead923bc3fb0c5d33224e75509
Xikaro <0regon.end@gmail.com>
2024-01-04 16:38:05 +0500

    material_sets

--- src/main/java/su/terrafirmagreg/core/compat/create/CreateCompat.java
+++ src/main/java/su/terrafirmagreg/core/compat/create/CreateCompat.java
@@ -5,6 +5,8 @@ import net.dries007.tfc.common.items.Food;
 import net.dries007.tfc.common.items.TFCItems;
 import net.minecraft.resources.ResourceLocation;
 
+import static su.terrafirmagreg.core.TerraFirmaGreg.MOD_ID;
+
 public class CreateCompat {
 
     public static void register() {
@@ -17,7 +19,7 @@ public class CreateCompat {
         // Все семяна имееют одинаковый урон
         for (var item : TFCItems.CROP_SEEDS.entrySet())
         {
-            new PotatoCannonProjectileType.Builder(new ResourceLocation("tfg", item.getKey().toString().toLowerCase() + "_seed"))
+            new PotatoCannonProjectileType.Builder(new ResourceLocation(MOD_ID, item.getKey().toString().toLowerCase() + "_seed"))
                     .damage(5)
                     .velocity(1.25F)
                     .knockback(1.5F)
@@ -25,63 +27,63 @@ public class CreateCompat {
                     .registerAndAssign(item.getValue().get());
         }
 
-        new PotatoCannonProjectileType.Builder(new ResourceLocation("tfg", "potato"))
+        new PotatoCannonProjectileType.Builder(new ResourceLocation(MOD_ID, "potato"))
                 .damage(9)
                 .velocity(1.25F)
                 .knockback(1.5F)
                 .renderTumbling()
                 .registerAndAssign(TFCItems.FOOD.get(Food.POTATO).get());
 
-        new PotatoCannonProjectileType.Builder(new ResourceLocation("tfg", "red_apple"))
+        new PotatoCannonProjectileType.Builder(new ResourceLocation(MOD_ID, "red_apple"))
                 .damage(8)
                 .velocity(1.25F)
                 .knockback(1.5F)
                 .renderTumbling()
                 .registerAndAssign(TFCItems.FOOD.get(Food.RED_APPLE).get());
 
-        new PotatoCannonProjectileType.Builder(new ResourceLocation("tfg", "plum"))
+        new PotatoCannonProjectileType.Builder(new ResourceLocation(MOD_ID, "plum"))
                 .damage(3)
                 .velocity(1.25F)
                 .knockback(1.5F)
                 .renderTumbling()
                 .registerAndAssign(TFCItems.FOOD.get(Food.PLUM).get());
 
-        new PotatoCannonProjectileType.Builder(new ResourceLocation("tfg", "orange"))
+        new PotatoCannonProjectileType.Builder(new ResourceLocation(MOD_ID, "orange"))
                 .damage(6)
                 .velocity(1.25F)
                 .knockback(1.5F)
                 .renderTumbling()
                 .registerAndAssign(TFCItems.FOOD.get(Food.ORANGE).get());
 
-        new PotatoCannonProjectileType.Builder(new ResourceLocation("tfg", "peach"))
+        new PotatoCannonProjectileType.Builder(new ResourceLocation(MOD_ID, "peach"))
                 .damage(2)
                 .velocity(1.25F)
                 .knockback(1.5F)
                 .renderTumbling()
                 .registerAndAssign(TFCItems.FOOD.get(Food.PEACH).get());
 
-        new PotatoCannonProjectileType.Builder(new ResourceLocation("tfg", "green_apple"))
+        new PotatoCannonProjectileType.Builder(new ResourceLocation(MOD_ID, "green_apple"))
                 .damage(8)
                 .velocity(1.25F)
                 .knockback(1.5F)
                 .renderTumbling()
                 .registerAndAssign(TFCItems.FOOD.get(Food.GREEN_APPLE).get());
 
-        new PotatoCannonProjectileType.Builder(new ResourceLocation("tfg", "cherry"))
+        new PotatoCannonProjectileType.Builder(new ResourceLocation(MOD_ID, "cherry"))
                 .damage(3)
                 .velocity(1.25F)
                 .knockback(1.5F)
                 .renderTumbling()
                 .registerAndAssign(TFCItems.FOOD.get(Food.CHERRY).get());
 
-        new PotatoCannonProjectileType.Builder(new ResourceLocation("tfg", "wintergreen"))
+        new PotatoCannonProjectileType.Builder(new ResourceLocation(MOD_ID, "wintergreen"))
                 .damage(2)
                 .velocity(1.25F)
                 .knockback(1.5F)
                 .renderTumbling()
                 .registerAndAssign(TFCItems.FOOD.get(Food.WINTERGREEN_BERRY).get());
 
-        new PotatoCannonProjectileType.Builder(new ResourceLocation("tfg", "onion"))
+        new PotatoCannonProjectileType.Builder(new ResourceLocation(MOD_ID, "onion"))
                 .damage(7)
                 .velocity(1.25F)
                 .knockback(1.5F)
