03a59511382dfbfa08302d4b1fd341de5a87cb9e
Dmitry <ondyshev.dmitry@gmail.com>
2024-01-12 10:14:01 +0700

    kjs plugin

---
+++ src/main/java/su/terrafirmagreg/core/compat/kjs/TFGKubeJSPlugin.java
@@ -0,0 +1,26 @@
+package su.terrafirmagreg.core.compat.kjs;
+
+import dev.latvian.mods.kubejs.KubeJSPlugin;
+import dev.latvian.mods.kubejs.script.BindingsEvent;
+import dev.latvian.mods.kubejs.script.ScriptType;
+import dev.latvian.mods.kubejs.util.ClassFilter;
+import su.terrafirmagreg.core.compat.gtceu.TFGMaterials;
+import su.terrafirmagreg.core.compat.gtceu.TFGPropertyKeys;
+import su.terrafirmagreg.core.compat.gtceu.TFGTagPrefixes;
+
+public class TFGKubeJSPlugin extends KubeJSPlugin {
+
+    @Override
+    public void registerClasses(ScriptType type, ClassFilter filter) {
+        super.registerClasses(type, filter);
+        filter.deny("su.terrafirmagreg.core");
+    }
+
+    @Override
+    public void registerBindings(BindingsEvent event) {
+        super.registerBindings(event);
+        event.add("TFGMaterials", TFGMaterials.class);
+        event.add("TFGTagPrefix", TFGTagPrefixes.class);
+        event.add("TFGPropertyKey", TFGPropertyKeys.class);
+    }
+}
