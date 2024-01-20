98c1eb345085dbea2503632ea15f97b5795dc367
Dmitry <ondyshev.dmitry@gmail.com>
2024-01-20 23:52:55 +0700

    я ухожу из моддинга

--- src/main/java/su/terrafirmagreg/core/compat/rei/TFGREIClientPlugin.java
+++ src/main/java/su/terrafirmagreg/core/compat/rei/TFGREIClientPlugin.java
@@ -14,11 +14,14 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import su.terrafirmagreg.core.TerraFirmaGreg;
 import su.terrafirmagreg.core.objects.TFGTags;
 
 import java.util.List;
 
+@OnlyIn(Dist.CLIENT)
 @REIPluginClient
 public class TFGREIClientPlugin implements REIClientPlugin {
 
