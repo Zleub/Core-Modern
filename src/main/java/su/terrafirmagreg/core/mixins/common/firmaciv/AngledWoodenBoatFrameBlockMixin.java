fa46132bf8412a77d038ab7ea47d631012f6025d
Dmitry <ondyshev.dmitry@gmail.com>
2024-01-28 21:44:51 +0700

    fuck

--- src/main/java/su/terrafirmagreg/core/mixins/common/firmaciv/AngledWoodenBoatFrameBlockMixin.java
+++ src/main/java/su/terrafirmagreg/core/mixins/common/firmaciv/AngledWoodenBoatFrameBlockMixin.java
@@ -7,13 +7,11 @@ import com.gregtechceu.gtceu.common.data.GTMaterials;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraftforge.items.ItemHandlerHelper;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import su.terrafirmagreg.core.objects.TFGTags;
-@Mixin(value = AngledWoodenBoatFrameBlock.class, remap = false)
+@Mixin(value = AngledWoodenBoatFrameBlock.class)
 public abstract class AngledWoodenBoatFrameBlockMixin {
 
     @Redirect(method = "use", at = @At(value = "INVOKE", target = "Lnet/minecraftforge/items/ItemHandlerHelper;giveItemToPlayer(Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/world/item/ItemStack;)V", ordinal = 1), remap = false)
