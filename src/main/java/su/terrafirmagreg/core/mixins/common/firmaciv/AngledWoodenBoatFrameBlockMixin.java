cdb6b5e18c7088e30804492ad46467c53da38a27
Dmitry <ondyshev.dmitry@gmail.com>
2024-01-28 21:41:39 +0700

    lol

--- src/main/java/su/terrafirmagreg/core/mixins/common/firmaciv/AngledWoodenBoatFrameBlockMixin.java
+++ src/main/java/su/terrafirmagreg/core/mixins/common/firmaciv/AngledWoodenBoatFrameBlockMixin.java
@@ -1,6 +1,9 @@
 package su.terrafirmagreg.core.mixins.common.firmaciv;
 
 import com.alekiponi.firmaciv.common.block.AngledWoodenBoatFrameBlock;
+import com.gregtechceu.gtceu.api.data.chemical.ChemicalHelper;
+import com.gregtechceu.gtceu.api.data.tag.TagPrefix;
+import com.gregtechceu.gtceu.common.data.GTMaterials;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
@@ -16,17 +19,14 @@ public abstract class AngledWoodenBoatFrameBlockMixin {
     @Redirect(method = "use", at = @At(value = "INVOKE", target = "Lnet/minecraftforge/items/ItemHandlerHelper;giveItemToPlayer(Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/world/item/ItemStack;)V", ordinal = 1), remap = false)
     private void onGiveCopperBoltToPlayer(Player player, ItemStack stack)
     {
-        var copperBoltStack = Ingredient.of(TFGTags.Items.CopperBolt).getItems()[0];
-
+        var copperBoltStack = ChemicalHelper.get(TagPrefix.bolt, GTMaterials.Copper);
         if (!copperBoltStack.isEmpty()) ItemHandlerHelper.giveItemToPlayer(player, copperBoltStack);
     }
 
     @Redirect(method = "use", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;is(Lnet/minecraft/world/item/Item;)Z", ordinal = 1))
     private boolean onCheckIsItemBolt(ItemStack instance, Item item)
     {
-        var copperBoltStack = Ingredient.of(TFGTags.Items.CopperBolt).getItems()[0];
-
+        var copperBoltStack = ChemicalHelper.get(TagPrefix.bolt, GTMaterials.Copper);
         return !copperBoltStack.isEmpty() && instance.is(copperBoltStack.getItem());
     }
-
 }
--- src/main/java/su/terrafirmagreg/core/mixins/common/firmaciv/WoodenBoatFrameBlockMixin.java
+++ src/main/java/su/terrafirmagreg/core/mixins/common/firmaciv/WoodenBoatFrameBlockMixin.java
@@ -1,15 +1,16 @@
 package su.terrafirmagreg.core.mixins.common.firmaciv;
 
 import com.alekiponi.firmaciv.common.block.FlatWoodenBoatFrameBlock;
+import com.gregtechceu.gtceu.api.data.chemical.ChemicalHelper;
+import com.gregtechceu.gtceu.api.data.tag.TagPrefix;
+import com.gregtechceu.gtceu.common.data.GTMaterials;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraftforge.items.ItemHandlerHelper;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import su.terrafirmagreg.core.objects.TFGTags;
 
 @Mixin(value = FlatWoodenBoatFrameBlock.class)
 public abstract class WoodenBoatFrameBlockMixin {
@@ -17,16 +18,14 @@ public abstract class WoodenBoatFrameBlockMixin {
     @Redirect(method = "use", at = @At(value = "INVOKE", target = "Lnet/minecraftforge/items/ItemHandlerHelper;giveItemToPlayer(Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/world/item/ItemStack;)V", ordinal = 1), remap = false)
     private void onGiveCopperBoltToPlayer(Player player, ItemStack stack)
     {
-        var copperBoltStack = Ingredient.of(TFGTags.Items.CopperBolt).getItems()[0];
-
+        var copperBoltStack = ChemicalHelper.get(TagPrefix.bolt, GTMaterials.Copper);
         if (!copperBoltStack.isEmpty()) ItemHandlerHelper.giveItemToPlayer(player, copperBoltStack);
     }
 
     @Redirect(method = "use", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;is(Lnet/minecraft/world/item/Item;)Z", ordinal = 1))
     private boolean onCheckIsItemBolt(ItemStack instance, Item item)
     {
-        var copperBoltStack = Ingredient.of(TFGTags.Items.CopperBolt).getItems()[0];
-
+        var copperBoltStack = ChemicalHelper.get(TagPrefix.bolt, GTMaterials.Copper);
         return !copperBoltStack.isEmpty() && instance.is(copperBoltStack.getItem());
     }
 
