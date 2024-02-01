41c1f1072508b43e7517a75eb6cc679d00385809
Xikaro <0regon.end@gmail.com>
2024-02-01 17:23:17 +0500

    EventHandlerMixin

---
+++ src/main/java/su/terrafirmagreg/core/mixins/client/sl/EventHandlerMixin.java
@@ -0,0 +1,23 @@
+package su.terrafirmagreg.core.mixins.client.sl;
+
+import net.minecraft.client.gui.screens.multiplayer.JoinMultiplayerScreen;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.client.event.ScreenEvent;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import top.seraphjack.simplelogin.client.EventHandler;
+
+@OnlyIn(Dist.CLIENT)
+@Mixin(value = EventHandler.class)
+public class EventHandlerMixin {
+
+	@Inject(method = "onGuiOpen", at = @At(value = "INVOKE", target = "Ltop/seraphjack/simplelogin/client/PasswordHolder;initialized()Z"), remap = false, cancellable = true)
+	private static void onGuiOpen(ScreenEvent.Opening event, CallbackInfo ci) {
+		if (!(event.getScreen() instanceof JoinMultiplayerScreen)) {
+			ci.cancel();
+		}
+	}
+}
