5b60fffd71bccc3557bb975c96db6b46a3e9644e
Dmitry <ondyshev.dmitry@gmail.com>
2024-03-21 23:33:04 +0700

    fuck mixins

--- src/main/java/su/terrafirmagreg/core/mixins/common/gtceu/FisherMachineMixin.java
+++ src/main/java/su/terrafirmagreg/core/mixins/common/gtceu/FisherMachineMixin.java
@@ -1,24 +1,41 @@
 package su.terrafirmagreg.core.mixins.common.gtceu;
 
+import com.gregtechceu.gtceu.api.machine.IMachineBlockEntity;
+import com.gregtechceu.gtceu.api.machine.TieredEnergyMachine;
+import com.gregtechceu.gtceu.api.machine.feature.IAutoOutputItem;
+import com.gregtechceu.gtceu.api.machine.feature.IFancyUIMachine;
+import com.gregtechceu.gtceu.api.machine.feature.IMachineModifyDrops;
 import com.gregtechceu.gtceu.common.machine.electric.FisherMachine;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
-import su.terrafirmagreg.core.objects.TFGTags;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = FisherMachine.class, remap = false)
 public class FisherMachineMixin {
 
+    // works fine
+    /*
+    @Inject(method = "updateFishingUpdateSubscription", at = @At(value = "HEAD"), remap = false)
+    private void asdasdasdas(CallbackInfo ci) {
+        System.out.println("dead");
+    }*/
+
+    // not working, WHY SUKAAAA
+    /*
     @Redirect(method = "updateFishingUpdateSubscription", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;is(Lnet/minecraft/world/item/Item;)Z"), remap = false)
     private boolean onUpdateFishingUpdateSubscription(ItemStack instance, Item pItem) {
-        return instance.is(TFGTags.Items.Strings);
+        return false;
     }
+    */
 
+    // not working, WHY SUKAAAA
+    /*
     @Redirect(method = "lambda$createBaitItemHandler$1", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;is(Lnet/minecraft/world/item/Item;)Z"), remap = false)
     private static boolean onCreateBaitItemHandler(ItemStack instance, Item pItem) {
-        return instance.is(TFGTags.Items.Strings);
+        return false;
     }
-
+    */
 }
