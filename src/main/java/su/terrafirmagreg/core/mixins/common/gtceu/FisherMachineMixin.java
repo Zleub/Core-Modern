994f1ef63c9ad03f96b2ec38d4a5784d0ead53c6
Dmitry <ondyshev.dmitry@gmail.com>
2024-03-22 13:58:36 +0700

    fisher fixes

--- src/main/java/su/terrafirmagreg/core/mixins/common/gtceu/FisherMachineMixin.java
+++ src/main/java/su/terrafirmagreg/core/mixins/common/gtceu/FisherMachineMixin.java
@@ -1,41 +1,64 @@
 package su.terrafirmagreg.core.mixins.common.gtceu;
 
 import com.gregtechceu.gtceu.api.machine.IMachineBlockEntity;
+import com.gregtechceu.gtceu.api.machine.TickableSubscription;
 import com.gregtechceu.gtceu.api.machine.TieredEnergyMachine;
-import com.gregtechceu.gtceu.api.machine.feature.IAutoOutputItem;
-import com.gregtechceu.gtceu.api.machine.feature.IFancyUIMachine;
-import com.gregtechceu.gtceu.api.machine.feature.IMachineModifyDrops;
+import com.gregtechceu.gtceu.api.machine.trait.NotifiableItemStackHandler;
 import com.gregtechceu.gtceu.common.machine.electric.FisherMachine;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import su.terrafirmagreg.core.objects.TFGTags;
+
+import java.util.function.Function;
 
 @Mixin(value = FisherMachine.class, remap = false)
-public class FisherMachineMixin {
+public abstract class FisherMachineMixin extends TieredEnergyMachine {
 
-    // works fine
-    /*
-    @Inject(method = "updateFishingUpdateSubscription", at = @At(value = "HEAD"), remap = false)
-    private void asdasdasdas(CallbackInfo ci) {
-        System.out.println("dead");
-    }*/
+    @Shadow @Final
+    protected NotifiableItemStackHandler baitHandler;
 
-    // not working, WHY SUKAAAA
-    /*
-    @Redirect(method = "updateFishingUpdateSubscription", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;is(Lnet/minecraft/world/item/Item;)Z"), remap = false)
-    private boolean onUpdateFishingUpdateSubscription(ItemStack instance, Item pItem) {
-        return false;
+    @Shadow
+    protected @Nullable TickableSubscription fishingSubs;
+
+    public FisherMachineMixin(IMachineBlockEntity holder, int tier, Object... args) {
+        super(holder, tier, args);
     }
-    */
 
-    // not working, WHY SUKAAAA
-    /*
-    @Redirect(method = "lambda$createBaitItemHandler$1", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;is(Lnet/minecraft/world/item/Item;)Z"), remap = false)
-    private static boolean onCreateBaitItemHandler(ItemStack instance, Item pItem) {
-        return false;
+    @Shadow
+    public boolean drainEnergy(boolean simulate) {
+        throw new AssertionError();
     }
-    */
+
+    @Shadow
+    public void fishingUpdate() {
+        throw new AssertionError();
+    }
+
+    @Inject(method = "updateFishingUpdateSubscription", at = @At(value = "HEAD"), cancellable = true, remap = false)
+    private void onUpdateFishingUpdateSubscription(CallbackInfo ci) {
+        if (drainEnergy(true) && baitHandler.getStackInSlot(0).is(TFGTags.Items.Strings)) {
+            fishingSubs = subscribeServerTick(fishingSubs, this::fishingUpdate);
+        } else if (fishingSubs != null) {
+            fishingSubs.unsubscribe();
+            fishingSubs = null;
+        }
+
+        ci.cancel();
+    }
+
+    /**
+     * Проверка на тег ниток, а не на только ванильные нитки
+     * */
+    @Redirect(method = "createBaitItemHandler", at = @At(value = "INVOKE", target = "Lcom/gregtechceu/gtceu/api/machine/trait/NotifiableItemStackHandler;setFilter(Ljava/util/function/Function;)Lcom/gregtechceu/gtceu/api/machine/trait/NotifiableItemStackHandler;"), remap = false)
+    private NotifiableItemStackHandler onCreateBaitItemHandler(NotifiableItemStackHandler instance, Function<ItemStack, Boolean> filter) {
+        return instance.setFilter((item) -> item.is(TFGTags.Items.Strings));
+    }
 }
