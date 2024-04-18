2bdd6e82cfa3c32e85dc904f439a70d85baed295
Dmitry <ondyshev.dmitry@gmail.com>
2024-04-18 18:58:04 +0700

    some jsons again

--- src/main/java/su/terrafirmagreg/core/TFGConfig.java
+++ src/main/java/su/terrafirmagreg/core/TFGConfig.java
@@ -8,11 +8,6 @@ public final class TFGConfig {
 
     @Configurable
     @Configurable.Synchronized
-    @Configurable.Comment("List of modids, which advancements has been excluded from game.")
-    public String[] modIds = new String[] { "minecraft", "data/tfc", "create", "gtceu" };
-
-    @Configurable
-    @Configurable.Synchronized
     @Configurable.Comment("Is TFC Compat Enabbled? Requied restart!")
     public boolean enableTFCAmbientCompat = true;
 }
