f4242eef12179b68589817498415c5c0663f467d
Dmitry <ondyshev.dmitry@gmail.com>
2024-04-29 14:18:57 +0700

    fixes

---
+++ src/main/java/su/terrafirmagreg/core/mixins/common/gtceu/PrimitivePumpMachineMixin.java
@@ -0,0 +1,17 @@
+package su.terrafirmagreg.core.mixins.common.gtceu;
+
+import com.gregtechceu.gtceu.common.machine.multiblock.primitive.PrimitivePumpMachine;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(PrimitivePumpMachine.class)
+public class PrimitivePumpMachineMixin {
+
+    @Inject(method = "produceWater", at = @At(value = "HEAD"), remap = false, cancellable = true)
+    private void produceWater(final CallbackInfo ci) {
+        ci.cancel();
+    }
+
+}
