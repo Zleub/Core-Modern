b778fcf93d2a1e8ec6b6817b4eb964094fa865ab
Dmitry <ondyshev.dmitry@gmail.com>
2024-04-30 22:24:04 +0700

    hi hi ha ha

---
+++ src/main/java/su/terrafirmagreg/core/mixins/common/ldlib/EMIPluginMixin.java
@@ -0,0 +1,17 @@
+package su.terrafirmagreg.core.mixins.common.ldlib;
+
+import com.lowdragmc.lowdraglib.emi.EMIPlugin;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(value = EMIPlugin.class, remap = false)
+public abstract class EMIPluginMixin {
+
+    @Inject(method = "isEmiEnabled", at = @At("HEAD"), remap = false, cancellable = true)
+    private static void tfg$isEmiEnabled(final CallbackInfoReturnable<Boolean> cir) {
+        cir.setReturnValue(true);
+    }
+
+}
