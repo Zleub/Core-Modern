ce1846469687c512de64a462fe59b30fee06cb6b
Dmitry <ondyshev.dmitry@gmail.com>
2024-05-01 21:28:57 +0700

    blyat fix

--- src/main/java/su/terrafirmagreg/core/mixins/common/gtceu/GTToolTypeMixin.java
+++ src/main/java/su/terrafirmagreg/core/mixins/common/gtceu/GTToolTypeMixin.java
@@ -2,13 +2,16 @@ package su.terrafirmagreg.core.mixins.common.gtceu;
 
 import com.gregtechceu.gtceu.api.item.tool.GTToolType;
 import com.gregtechceu.gtceu.api.item.tool.ToolDefinitionBuilder;
+import com.gregtechceu.gtceu.api.item.tool.behavior.IToolBehavior;
 import com.gregtechceu.gtceu.common.item.tool.behavior.HarvestIceBehavior;
-import com.gregtechceu.gtceu.common.item.tool.behavior.HoeGroundBehavior;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import su.terrafirmagreg.core.TerraFirmaGreg;
 import su.terrafirmagreg.core.compat.gtceu.behaviors.CanoeCreatorBehavior;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.function.UnaryOperator;
 
 @Mixin(value = GTToolType.class, remap = false)
@@ -16,11 +19,9 @@ public abstract class GTToolTypeMixin {
 
     /**
      * Устанавливает новое поведение для пилы, чтобы та могла создавать лодки каное из FirmaCiv.
-     * */
-    @Redirect(method = "<clinit>", at = @At(value = "INVOKE", target = "Lcom/gregtechceu/gtceu/api/item/tool/GTToolType$Builder;toolStats(Ljava/util/function/UnaryOperator;)Lcom/gregtechceu/gtceu/api/item/tool/GTToolType$Builder;", ordinal = 3), remap = false)
-    private static GTToolType.Builder tfg$clinit(GTToolType.Builder instance, UnaryOperator<ToolDefinitionBuilder> builder) {
-        return instance.toolStats(b -> b.crafting().damagePerCraftingAction(2)
-                .attackDamage(-1.0F).attackSpeed(-2.6F)
-                .behaviors(HarvestIceBehavior.INSTANCE, CanoeCreatorBehavior.INSTANCE));
+     */
+    @Redirect(method = "lambda$static$8", at = @At(value = "INVOKE", target = "Lcom/gregtechceu/gtceu/api/item/tool/ToolDefinitionBuilder;behaviors([Lcom/gregtechceu/gtceu/api/item/tool/behavior/IToolBehavior;)Lcom/gregtechceu/gtceu/api/item/tool/ToolDefinitionBuilder;"), remap = false)
+    private static ToolDefinitionBuilder tfg$clinit(ToolDefinitionBuilder instance, IToolBehavior[] behaviours) {
+        return instance.behaviors(HarvestIceBehavior.INSTANCE, CanoeCreatorBehavior.INSTANCE);
     }
 }
