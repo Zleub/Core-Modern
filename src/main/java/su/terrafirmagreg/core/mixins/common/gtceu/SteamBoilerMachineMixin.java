a2c4dbdf6bf16c44e446a9fc1ac90ad3cf3547a4
Dmitry <ondyshev.dmitry@gmail.com>
2024-05-02 23:10:04 +0700

    fix boilers

---
+++ src/main/java/su/terrafirmagreg/core/mixins/common/gtceu/SteamBoilerMachineMixin.java
@@ -0,0 +1,22 @@
+package su.terrafirmagreg.core.mixins.common.gtceu;
+
+import com.gregtechceu.gtceu.api.machine.steam.SteamBoilerMachine;
+import com.gregtechceu.gtceu.api.machine.trait.NotifiableFluidTank;
+import com.lowdragmc.lowdraglib.side.fluid.FluidStack;
+import net.dries007.tfc.common.fluids.TFCFluids;
+import net.minecraft.world.level.material.Fluids;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import su.terrafirmagreg.core.TerraFirmaGreg;
+
+import java.util.function.Predicate;
+
+@Mixin(value = SteamBoilerMachine.class, remap = false)
+public abstract class SteamBoilerMachineMixin {
+
+    @Redirect(method = "<init>", at = @At(value = "INVOKE", target = "Lcom/gregtechceu/gtceu/api/machine/trait/NotifiableFluidTank;setFilter(Ljava/util/function/Predicate;)Lcom/gregtechceu/gtceu/api/machine/trait/NotifiableFluidTank;"), remap = false)
+    private NotifiableFluidTank tfg$init(NotifiableFluidTank instance, Predicate<FluidStack> fluidStackPredicate) {
+        return instance.setFilter(fluidStack -> ((TFCFluids.RIVER_WATER.get() == fluidStack.getFluid()) || (Fluids.WATER == fluidStack.getFluid())));
+    }
+}
