c64091bab0e75b35eb00f4f11ae86ea660ee6be6
Dmitry <ondyshev.dmitry@gmail.com>
2024-05-25 10:41:35 +0700

    port oregen from 0.8.0

--- src/main/java/su/terrafirmagreg/core/compat/gtceu/TFGWorldGenLayers.java
+++ src/main/java/su/terrafirmagreg/core/compat/gtceu/TFGWorldGenLayers.java
@@ -1,22 +1,25 @@
 package su.terrafirmagreg.core.compat.gtceu;
 
+import com.gregtechceu.gtceu.api.data.worldgen.IWorldGenLayer;
 import com.gregtechceu.gtceu.integration.kjs.builders.WorldGenLayerBuilder;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.levelgen.structure.templatesystem.RuleTest;
 import net.minecraft.world.level.levelgen.structure.templatesystem.TagMatchTest;
 import su.terrafirmagreg.core.TerraFirmaGreg;
-import su.terrafirmagreg.core.objects.TFGTags;
+import su.terrafirmagreg.core.objects.data.TFGTags;
 
 public final class TFGWorldGenLayers {
 
-    public static final RuleTest EARTH_ORE_REPLACEABLES = new TagMatchTest(TFGTags.Blocks.RawRockBlocks);
+    public static final RuleTest ALL_STONETYPES_EARTH = new TagMatchTest(TFGTags.Blocks.RawRockBlocks);
+
+    public static final IWorldGenLayer EARTH_GEN_LAYER = new WorldGenLayerBuilder(TerraFirmaGreg.id("earth"))
+            .dimensions(Level.OVERWORLD.location())
+            .targets(() -> ALL_STONETYPES_EARTH)
+            .register();
 
     public static void init() {
-        new WorldGenLayerBuilder(new ResourceLocation(TerraFirmaGreg.MOD_ID, "earth"))
-            .dimensions(Level.OVERWORLD.location())
-            .targets(() -> EARTH_ORE_REPLACEABLES)
-            .register();
+
     }
 
 }
