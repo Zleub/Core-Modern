50cbfa1eac003e3bd39785772269293c302343e7
Dmitry <ondyshev.dmitry@gmail.com>
2024-05-25 10:38:51 +0700

    cleanup tfc + create mixins

--- src/main/java/su/terrafirmagreg/core/mixins/common/tfc/CharcoalForgeBlockMixin.java
+++ src/main/java/su/terrafirmagreg/core/mixins/common/tfc/CharcoalForgeBlockMixin.java
@@ -6,7 +6,6 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
@@ -20,7 +19,7 @@ public abstract class CharcoalForgeBlockMixin {
 
     @Inject(method = "use", at = @At("HEAD"), cancellable = true)
     public void tfg$use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hit, CallbackInfoReturnable<InteractionResult> cir) {
-        ItemStack heldItem = player.getItemInHand(hand);
+        var heldItem = player.getItemInHand(hand);
         if (!heldItem.isEmpty() && heldItem.getItem() instanceof ArmItem) {
             cir.setReturnValue(InteractionResult.FAIL);
         }
