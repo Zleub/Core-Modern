5586044c5e1c2b15bd656f00e5c6283a5d0f78a4
Dmitry <ondyshev.dmitry@gmail.com>
2024-06-15 11:47:13 +0700

    forgot ceh

--- src/main/java/su/terrafirmagreg/core/common/TFGCommonEventHandler.java
+++ src/main/java/su/terrafirmagreg/core/common/TFGCommonEventHandler.java
@@ -1,16 +1,20 @@
-package su.terrafirmagreg.core;
+package su.terrafirmagreg.core.common;
 
 import com.gregtechceu.gtceu.api.GTCEuAPI;
 import com.gregtechceu.gtceu.api.data.chemical.material.event.MaterialEvent;
 import com.gregtechceu.gtceu.api.data.chemical.material.event.MaterialRegistryEvent;
 import com.gregtechceu.gtceu.api.data.chemical.material.event.PostMaterialEvent;
+import com.gregtechceu.gtceu.data.pack.GTPackSource;
+import net.minecraft.server.packs.PackType;
+import net.minecraft.server.packs.repository.Pack;
+import net.minecraftforge.event.AddPackFindersEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import su.terrafirmagreg.core.compat.create.CreateCompat;
+import su.terrafirmagreg.core.TFGConfig;
+import su.terrafirmagreg.core.TFGCore;
+import su.terrafirmagreg.core.common.data.TFGDynamicDataPack;
 import su.terrafirmagreg.core.compat.gtceu.materials.TFGMaterialHandler;
-import su.terrafirmagreg.core.compat.tfcambiental.TFCAmbientalCompat;
-import su.terrafirmagreg.core.objects.TFGRegistries;
 
 public final class TFGCommonEventHandler {
 
@@ -22,11 +26,11 @@ public final class TFGCommonEventHandler {
         bus.addListener(TFGCommonEventHandler::onRegisterMaterialRegistry);
         bus.addListener(TFGCommonEventHandler::onRegisterMaterials);
         bus.addListener(TFGCommonEventHandler::onPostRegisterMaterials);
-        // bus.addListener(TFGCommonEventHandler::registerPackFinders);
+        bus.addListener(TFGCommonEventHandler::registerPackFinders);
     }
 
     private static void onRegisterMaterialRegistry(final MaterialRegistryEvent event) {
-        TFGRegistries.MATERIAL_REGISTRY = GTCEuAPI.materialManager.createRegistry(TerraFirmaGreg.MOD_ID);
+        TFGCore.MATERIAL_REGISTRY = GTCEuAPI.materialManager.createRegistry(TFGCore.MOD_ID);
     }
 
     private static void onRegisterMaterials(final MaterialEvent event) {
@@ -39,18 +43,16 @@ public final class TFGCommonEventHandler {
 
     private static void onCommonSetup(final FMLCommonSetupEvent event) {
         event.enqueueWork(() -> {
-            if (TFGConfig.enableTFCAmbientalCompat) TFCAmbientalCompat.register();
-            if (TFGConfig.enableCreateCompat) CreateCompat.register();
+            //            if (TFGConfig.enableTFCAmbientalCompat) TFCAmbientalCompat.register();
+            //            if (TFGConfig.enableCreateCompat) CreateCompat.register();
         });
     }
 
-    // private static void registerPackFinders(final AddPackFindersEvent event) {
-    //     if (event.getPackType() == PackType.CLIENT_RESOURCES) return;
-    //
-    //    event.addRepositorySource(new GTPackSource("tfg:dynamic_data",
-    //            event.getPackType(),
-    //             Pack.Position.BOTTOM,
-    //            TFGDynamicDataPack::new)
-    //    );
-    // }
+     private static void registerPackFinders(final AddPackFindersEvent event) {
+         if (event.getPackType() == PackType.CLIENT_RESOURCES) return;
+
+        event.addRepositorySource(
+                new GTPackSource("tfg:dynamic_data", event.getPackType(), Pack.Position.BOTTOM, TFGDynamicDataPack::new)
+        );
+     }
 }
