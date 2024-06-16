6aaad8326e750aa68e0be39bfabfa30f81260c5a
Dmitry <ondyshev.dmitry@gmail.com>
2024-06-16 12:11:02 +0700

    залупа

--- src/main/java/su/terrafirmagreg/core/mixins/common/gtceu/MixinHelpersMixin.java
+++ src/main/java/su/terrafirmagreg/core/mixins/common/gtceu/MixinHelpersMixin.java
@@ -69,35 +69,4 @@ public abstract class MixinHelpersMixin {
         );
     }
 
-//    @Redirect(method = "lambda$generateGTDynamicLoot$23", at = @At(value = "INVOKE", target = "Lnet/minecraft/data/loot/BlockLootSubProvider;createSilkTouchDispatchTable(Lnet/minecraft/world/level/block/Block;Lnet/minecraft/world/level/storage/loot/entries/LootPoolEntryContainer$Builder;)Lnet/minecraft/world/level/storage/loot/LootTable$Builder;"), remap = false)
-//    private static LootTable.Builder tfg$lambda$generateGTDynamicLoot$23(Block pBlock, LootPoolEntryContainer.Builder<?> pBuilder, @Local int oreMultiplier, @Local Block block, @Local ItemStack dropItem) {
-//        return LootTable.lootTable().withPool(
-//                LootPool.lootPool()
-//                        .setRolls(ConstantValue.exactly(1.0F))
-//                        .add(
-//                                AlternativesEntry.alternatives(
-//                                                LootItem.lootTableItem(Items.ACACIA_LEAVES)
-//                                                        .apply(SetItemCountFunction.setCount(UniformGenerator.between(1, Math.max(1, oreMultiplier))))
-//                                                        .setWeight(20)
-//                                                        .when(IBlockLootSubProviderAccessor.getHasNoSilkTouchCondition())
-//                                                        .apply(ApplyExplosionDecay.explosionDecay()),
-//                                                LootItem.lootTableItem(Items.STONE)
-//                                                        .apply(SetItemCountFunction.setCount(UniformGenerator.between(1, Math.max(1, oreMultiplier))))
-//                                                        .setWeight(60)
-//                                                        .when(IBlockLootSubProviderAccessor.getHasNoSilkTouchCondition())
-//                                                        .apply(ApplyExplosionDecay.explosionDecay()),
-//                                                LootItem.lootTableItem(Items.OAK_BOAT)
-//                                                        .apply(SetItemCountFunction.setCount(UniformGenerator.between(1, Math.max(1, oreMultiplier))))
-//                                                        .setWeight(20)
-//                                                        .when(IBlockLootSubProviderAccessor.getHasNoSilkTouchCondition())
-//                                                        .apply(ApplyExplosionDecay.explosionDecay())
-//                                        )
-//                                        .otherwise(
-//                                                LootItem.lootTableItem(block)
-//                                                        .apply(SetItemCountFunction.setCount(UniformGenerator.between(0, 1)))
-//                                        )
-//                        )
-//        );
-//    }
-
 }
