b716d5f289b7f92b428f3c5e5af557653895b8e6
Dmitry <ondyshev.dmitry@yandex.ru>
2024-09-13 08:00:18 +0700

    inject defaultconfigs inside every world

--- src/main/java/su/terrafirmagreg/core/TFGCore.java
+++ src/main/java/su/terrafirmagreg/core/TFGCore.java
@@ -5,11 +5,13 @@ import com.gregtechceu.gtceu.api.registry.registrate.GTRegistrate;
 import com.mojang.logging.LogUtils;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.fml.IExtensionPoint;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.loading.FMLEnvironment;
+import net.minecraftforge.network.NetworkConstants;
 import org.slf4j.Logger;
 import su.terrafirmagreg.core.client.TFGClientEventHandler;
 import su.terrafirmagreg.core.common.TFGCommonEventHandler;
@@ -28,6 +30,8 @@ public final class TFGCore {
 
         TFGCommonEventHandler.init();
         if (FMLEnvironment.dist == Dist.CLIENT) TFGClientEventHandler.init();
+
+        setupFixForGlobalServerConfig();
     }
 
     public static boolean IsAe2Loaded() {
@@ -60,5 +64,12 @@ public final class TFGCore {
 
     public static ResourceLocation id(String name) {
         return new ResourceLocation(MOD_ID, name);
+    }
+
+    private static void setupFixForGlobalServerConfig() {
+        ModLoadingContext.get().registerExtensionPoint(
+                IExtensionPoint.DisplayTest.class,
+                () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true)
+        );
     }
 }
